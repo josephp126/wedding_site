8debaa20a8f17e29331a68958dfe030c678ce988
winner <linodeBp17@gmail.com>
2021-08-26 00:51:37 -0500

    config mail update

--- config/mail.php
+++ config/mail.php
@@ -66,7 +66,7 @@ return [
     |
     */
 
-    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+    'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
 
     /*
     |--------------------------------------------------------------------------
