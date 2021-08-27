91ff1a70da54a034eab169bb01ade3c1ae813287
winner <linodeBp17@gmail.com>
2021-08-26 23:03:31 -0500

    change style

--- resources/views/pages/buyInvitation.blade.php
+++ resources/views/pages/buyInvitation.blade.php
@@ -19,10 +19,12 @@
             </div>
         </div>
         <div class="invitation-content" align="center" style="background: url('{{$template_url}}') no-repeat center center;">
-            <p>{{$bride_name}} & {{$groom_name}}</p>
-            <p>{{ trans('message.save_date') }}</p>
-            <p>{{ $marriage_date }}</p>
-            <p>{{ $message_guest }}</p>
+            <div class="invitation-detail">
+                <p>{{$bride_name}} & {{$groom_name}}</p>
+                <p>{{ trans('message.save_date') }}</p>
+                <p>{{ $marriage_date }}</p>
+                <p>{{ $message_guest }}</p>
+            </div>
             <div class="share-buttons" align="center">
                 <img src="{{asset('assets/images/gmail_icon.png')}}"/>
                 <img src="{{asset('assets/images/telegram_icon.png')}}"/>
@@ -59,17 +61,26 @@
             $('.invitation-content p').css('font-size', '27px');
             $('.invitation-content p:last-child').css('font-size', '18px');
             $('.invitation-content p:first-child').css('margin-top', '90%');
+            $('.invitation-detail').css('width', '100%');
+            $('.share-buttons img').css('width', '40px');
+            $('.share-buttons img:last-child').css('width', '34px');
             $('.hidden-xs').css('display', 'none');
         } else if($(window).width() > 1366){
             $('.invitation-content p').css('font-size', '44px');
             $('.invitation-content p:last-child').css('font-size', '26px');
             $('.invitation-content p:first-child').css('margin-top', '11%');
+            $('.invitation-detail').css('width', '50%');
+            $('.share-buttons img').css('width', '64px');
+            $('.share-buttons img:last-child').css('width', '53px');
             $('.hidden-xs').css('display', 'block');
         }
         else{
             $('.invitation-content p').css('font-size', '44px');
             $('.invitation-content p:last-child').css('font-size', '26px');
             $('.invitation-content p:first-child').css('margin-top', '18%');
+            $('.invitation-detail').css('width', '50%');
+            $('.share-buttons img').css('width', '64px');
+            $('.share-buttons img:last-child').css('width', '53px');
             $('.hidden-xs').css('display', 'block');
         }
         $(window).on('resize', function(){
@@ -79,17 +90,26 @@
                 $('.invitation-content p:last-child').css('font-size', '18px');
                 $('.invitation-content p:first-child').css('margin-top', '90%');
                 $('.hidden-xs').css('display', 'none');
+                $('.invitation-detail').css('width', '100%');
+                $('.share-buttons img').css('width', '40px');
+                $('.share-buttons img:last-child').css('width', '34px');
             } else if(win.width() > 1366){
                 $('.invitation-content p').css('font-size', '44px');
                 $('.invitation-content p:last-child').css('font-size', '26px');
                 $('.invitation-content p:first-child').css('margin-top', '11%');
                 $('.hidden-xs').css('display', 'block');
+                $('.invitation-detail').css('width', '50%');
+                $('.share-buttons img').css('width', '64px');
+                $('.share-buttons img:last-child').css('width', '53px');
             }
             else{
                 $('.invitation-content p').css('font-size', '44px');
                 $('.invitation-content p:last-child').css('font-size', '26px');
                 $('.invitation-content p:first-child').css('margin-top', '18%');
                 $('.hidden-xs').css('display', 'block');
+                $('.invitation-detail').css('width', '50%');
+                $('.share-buttons img').css('width', '64px');
+                $('.share-buttons img:last-child').css('width', '53px');
             }
         });
         $('.pay-button').on('click', '#pay_button', function(){
